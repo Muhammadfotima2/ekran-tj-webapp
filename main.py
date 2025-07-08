0cb167f44acd7d9c735eaf8015024625046d0a29
Muhammadfotima2 <Muhammadfotima24@gmail.com>
2025-07-08 17:24:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -8,8 +7,3 @@ def index():
     if request.method == 'POST':
         return "POST-запрос получен"
     return render_template('catalog.html')
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
-
