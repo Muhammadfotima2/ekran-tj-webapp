f8c3fc06465072946a9402b8944e10357412a0b8
Muhammadfotima2 <Muhammadfotima24@gmail.com>
2025-07-08 16:18:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        return "POST-запрос получен"
     return render_template('catalog.html')
 
 if __name__ == '__main__':
